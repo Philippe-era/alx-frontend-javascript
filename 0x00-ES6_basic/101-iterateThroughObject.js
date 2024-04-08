30f4a53b8066a96a18eb55a249f426a20530cfd3
= <=>
2024-04-08 13:47:06 -0700

    can i go sleep

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-	let workers = '';
-	for (const item of reportWithIterator) {
-		workers += `${item} | `;
-	}
-	return workers.slice(0, -3);
-}
-
-
+  return [...reportWithIterator].join(' | ');
+}}
