138442656c7c95219d32ac37fc8093794dda8997
= <=>
2024-04-08 04:45:18 -0700

    sum

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+export default function taskBlock(trueOrFalse) {
+  let first_t = false;
+  const second_t = true;
+
+  if (trueOrFalse) {
+    first_t = true;
+   
+  }
+
+  return [first_t, second_t];
+}
+
+
