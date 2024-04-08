a3b8119443dcf8afcebbacafdddb962fbf475282
= <=>
2024-04-08 04:14:08 -0700

    one

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+export default function taskBlock(validate) {
+  let first_t = false;
+  const second_t = true;
+
+  if (validate) {
+    first_t = true;
+   
+  }
+
+  return [first_t, second_t];
+}
+
