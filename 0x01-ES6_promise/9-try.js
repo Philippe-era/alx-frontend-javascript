3197d3a8bcee77192247daed1d2dadd9a9029320
= <=>
2024-04-10 08:48:47 -0700

    fixing

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,12 @@
-export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error();
-    }
-    return numerator / denominator;
-  } catch (error) {
-    throw Error('cannot divide by 0');
-  }
-}
+export default function guardrail(mathFunction) {
+  const equation = [];
 
+  try {
+    equation.push(mathFunction());
+  } catch (err) {
+    equation.push(err.toString());
+  } finally {
+    equation.push('Guardrail was processed');
+  }
+  return equation;
+}
