c16d7113bab281e1e616b4166ea862535222c8bb
= <=>
2024-04-11 06:46:56 -0700

    classes

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,48 +1,16 @@
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(value) {
-    this._brand = value;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value) {
-    this._color = value;
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    const clone_car = Object.create(Car.prototype);
-    const symbols = Object.getOwnPropertySymbols(this);
-
-    symbols.forEach(symbol => {
-      const describe = Object.getOwnPropertyDescriptor(this, symbol);
-      Object.defineProperty(clone_car, symbol, describe);
-    });
-
-    return clone_car;
+    const new_car = this.constructor[Symbol.species];
+    return new new_car();
   }
 }
-
-export default Car;
-
-
